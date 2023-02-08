4d5655922c1950254f87bf8c78dc1f4ff4e68908
Shilpa Kancharla <shilpakancharla18@gmail.com>
2023-02-08 12:21:33 -0800

    Update LISABuilder.py

--- tutorials/LISA/LISABuilder.py
+++ tutorials/LISA/LISABuilder.py
@@ -4,10 +4,8 @@ import pathlib
 import numpy as np
 import pandas as pd
 
-import keras
-import keras_cv
 import tensorflow as tf
-import tensorflow_datasets as tfds 
+import tensorflow_datasets as tfds
 
 class LISABuilder(tfds.core.GeneratorBasedBuilder):
   """
@@ -36,7 +34,7 @@ class LISABuilder(tfds.core.GeneratorBasedBuilder):
         }),
         disable_shuffling=False
     )
-  
+
   def train_test_split(self, img_labels_dict, training_percent = 0.80):
     # Shuffle the dictionary by the keys
     keys = list(img_labels_dict.keys())
@@ -51,72 +49,86 @@ class LISABuilder(tfds.core.GeneratorBasedBuilder):
 
     return training_dict, test_dict
 
-  def format_bounding_boxes(self, upper_left_x, upper_left_y, lower_right_x, lower_right_y):
+  def format_bounding_boxes(self, upper_left_x, upper_left_y, 
+                            lower_right_x, lower_right_y, 
+                            height, width):
     """
       Package the coordinates into a tf.Tensor.
     """
-    # Return ymin, xmin, ymax, xmax - yxyx format
-    return np.asarray([upper_left_y, upper_left_x, lower_right_y, lower_right_x])
-  
+    # Return ymin, xmin, ymax, xmax - yxyx format divided by height and width
+    ymin = upper_left_y / height
+    xmin = upper_left_x / width
+    ymax = lower_right_y / height
+    xmax = lower_right_x / width
+    return np.asarray([ymin, xmin, ymax, xmax])
+
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    download_annot_dir = pathlib.Path('./LISA/annotations/')
-    download_img_dir = pathlib.Path('./LISA/images/')
+    annot_path = pathlib.Path('./LISA_processed/annotations/')
+    img_path = pathlib.Path('./LISA_processed/images/')
     classes = ['stop', 'go', 'warning', 'warningLeft', 'stopLeft', 'goForward', 'goLeft']
     classes = sorted(classes)
     class_mapping = dict((name, idx) for idx, name in enumerate(classes))
 
-    image_labels_dict = dict()
-    for f in download_img_dir.rglob("*/*.jpg"):
-      for a in download_annot_dir.rglob("*/*.csv"):
-        if str(a).split('/')[-2] in str(f):
-          image_labels_dict[str(f)] = str(a)
+    data_dict = dict() # Let each image have their entries for the bbox coordinates and classes
+    for img_file in img_path.glob("*.jpg"):
+      for annot_file in annot_path.glob("*.csv"):
+        # Turn the .csv file into a dataframe for easy reading
+        df = pd.read_csv(str(annot_file), delimiter=',')
+        if any(img_file.name == name for name in df['Filename'].unique()):
+          # Get indices of matched up file names 
+          idx = df.index[df['Filename'] == img_file.name].tolist()
+          upper_left_x, upper_left_y, lower_right_x, lower_right_y, classes = [], [], [], [], []
+          for i in idx:
+            # Get the bounding box information: upper_left_x, upper_left_y,	lower_right_x	, lower_right_y
+            upper_left_x.append(df.loc[i, 'Upper left corner X']) # xmin
+            upper_left_y.append(df.loc[i, 'Upper left corner Y']) # ymin
+            lower_right_x.append(df.loc[i, 'Lower right corner X']) # xmax
+            lower_right_y.append(df.loc[i, 'Lower right corner Y']) # ymax
+            # Get the class information
+            classes.append(df.loc[i, 'Annotation tag'])
+          # Ensure all new lists are the same length - sanity check
+          assert len(upper_left_x) == len(upper_left_y) == len(lower_right_x) == len(lower_right_y) == len(classes)
+          data_dict[img_file.name] = {'xmin': upper_left_x,
+                                      'ymin': upper_left_y,
+                                      'xmax': lower_right_x,
+                                      'ymax': lower_right_y,
+                                      'classes': classes}
 
-    training_dict, test_dict = self.train_test_split(image_labels_dict)
+    training_dict, test_dict = self.train_test_split(data_dict)
 
     return {
-        'train': self._generate_examples(training_dict, # Specify training dictionary
-                                          class_mapping), 
-        'test': self._generate_examples(test_dict, # Specify test dictionary
-                                        class_mapping) 
+        'train': self._generate_examples(training_dict, class_mapping),
+        'test': self._generate_examples(test_dict, class_mapping)
     }
 
-  def _generate_examples(self, data_dictionary, class_mapping):
+  def _generate_examples(self, data_dict, class_mapping):
     image_id = 0
-    for key in data_dictionary:
-      image = PIL.Image.open(key)
+    for key in data_dict:
+      image = PIL.Image.open('./LISA_processed/images/' + key)
       image_tensor = np.asarray(image)
       image_id += 1
 
       height, width, _ = image_tensor.shape
 
-      # Drop unecessary rows from dataframe, this will save you some memory
-      df = pd.read_csv(data_dictionary[key],
-                       delimiter=';').drop(['Origin file',
-                                            'Origin frame number',
-                                            'Origin track',
-                                            'Origin track frame number'], axis=1)
+      bbox_coords, cls = [], []
+      # Recall the value of this dictionary is also a dictionary
+      for i in range(len(data_dict[key]['xmin'])): # All lists are same length, does not matter which one you choose
+        # Get each individual coordinate and corresponding class
+        xmin = data_dict[key]['xmin'][i]
+        ymin = data_dict[key]['ymin'][i]
+        xmax = data_dict[key]['xmax'][i]
+        ymax = data_dict[key]['ymax'][i]
+        # Format the bounding box coordinates
+        bbox_coords.append(self.format_bounding_boxes(xmin, ymin, 
+                                                      xmax, ymax, 
+                                                      height, width))
+        cls.append(class_mapping[data_dict[key]['classes'][i]])
 
-      # Get the dataframe row that has the 'Filename' as the key
-      row = df.loc[df['Filename'].str.contains(key.split('/')[-1])]
-
-      if len(row) > 0:
-        cls, bbox_coords = [], []
-        for idx, row in row.iterrows(): # Iterate over the rows of the dataframe
-          class_ = row['Annotation tag']
-          upper_left_x = row['Upper left corner X'] # xmin
-          upper_left_y = row['Upper left corner Y'] # ymin
-          lower_right_x = row['Lower right corner X'] # xmax
-          lower_right_y = row['Lower right corner Y'] # ymax
-          cls.append(class_mapping[class_])
-          bbox_coords.append(self.format_bounding_boxes(upper_left_x / width,
-                                                        upper_left_y / height,
-                                                        lower_right_x / width,
-                                                        lower_right_y / height))
-        example = {'image': image_tensor,
-                   'bounding boxes': {
+      example = {'image': image_tensor,
+                 'bounding boxes': {
                      'bbox': bbox_coords,
                      'label': cls
-                     }
-                  }
+                 }
+                }
 
-        yield image_id, example
+      yield image_id, example  
